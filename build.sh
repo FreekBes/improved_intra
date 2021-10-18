928de125d2092e8162e7c0b61b0efe85f1e82071
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2021-10-19 00:15:00 +0200

    added Windows steps for build script

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
-zip -r chromium.zip manifest.json LICENSE dark.css images/logo*.png
-mv manifest.json manifest-chromium.json
-mv manifest-ff.json manifest.json
-zip -r firefox.zip manifest.json LICENSE dark.css images/logo*.png
-mv manifest.json manifest-ff.json
-mv manifest-chromium.json manifest.json
+#!/bin/bash
+if ! [ command -v zip &> /dev/null ]; then
+	tar.exe -a -c -f chromium.zip manifest.json LICENSE dark.css images/logo*.png _locales/*
+	mv manifest.json manifest-chromium.json
+	mv manifest-ff.json manifest.json
+	tar.exe -a -c -f firefox.zip manifest.json LICENSE dark.css images/logo*.png _locales/*
+	mv manifest.json manifest-ff.json
+	mv manifest-chromium.json manifest.json
+else
+	zip -r chromium.zip manifest.json LICENSE dark.css images/logo*.png _locales/*
+	mv manifest.json manifest-chromium.json
+	mv manifest-ff.json manifest.json
+	zip -r firefox.zip manifest.json LICENSE dark.css images/logo*.png _locales/*
+	mv manifest.json manifest-ff.json
+	mv manifest-chromium.json manifest.json
+fi
