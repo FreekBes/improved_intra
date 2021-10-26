27fd752f161dfe436daa3b7fadee6cea4d45dcaa
Freek Bes <freekbladnl@gmail.com>
2021-10-27 00:50:39 +0200

    changed if statement for os detection

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-if ! [ command -v zip &> /dev/null ]; then
-	tar.exe -a -c -f chromium.zip manifest.json LICENSE dark.css images/logo*.png _locales/*
-	mv manifest.json manifest-chromium.json
-	mv manifest-ff.json manifest.json
-	tar.exe -a -c -f firefox.zip manifest.json LICENSE dark.css images/logo*.png _locales/*
-	mv manifest.json manifest-ff.json
-	mv manifest-chromium.json manifest.json
-else
+if [[ $OSTYPE == 'darwin'* ]]; then
 	zip -r chromium.zip manifest.json LICENSE dark.css images/logo*.png _locales/*
 	mv manifest.json manifest-chromium.json
 	mv manifest-ff.json manifest.json
 	zip -r firefox.zip manifest.json LICENSE dark.css images/logo*.png _locales/*
+	mv manifest.json manifest-ff.json
+	mv manifest-chromium.json manifest.json
+else
+	tar.exe -a -c -f chromium.zip manifest.json LICENSE dark.css images/logo*.png _locales/*
+	mv manifest.json manifest-chromium.json
+	mv manifest-ff.json manifest.json
+	tar.exe -a -c -f firefox.zip manifest.json LICENSE dark.css images/logo*.png _locales/*
 	mv manifest.json manifest-ff.json
 	mv manifest-chromium.json manifest.json
 fi
