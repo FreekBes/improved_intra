329080c59cf9cf114d0945571f64888af6fecb69
Freek Bes <freekbladnl@gmail.com>
2021-11-13 01:19:12 +0100

    fixed dashboard page but only for codam

--- improv.js
+++ improv.js
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/13 00:37:55 by fbes          #+#    #+#                 */
-/*   Updated: 2021/11/13 01:09:50 by fbes          ########   odam.nl         */
+/*   Updated: 2021/11/13 01:18:41 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ if (window.location.pathname.indexOf("/users/") == 0 || (window.location.hostnam
 	var userPosteInfos = document.getElementsByClassName("user-poste-infos");
 	var supportedCampuses = ["Amsterdam", "Paris"];
 
-	if (userPosteInfos.length > 0 && supportedCampuses.indexOf(getCampus()) > -1) {
+	if (userPosteInfos.length > 0 && (supportedCampuses.indexOf(getCampus()) > -1 || userPosteInfos[0].textContent.indexOf(".codam.nl") > -1)) {
 		userPosteInfos[0].style.cursor = "pointer";
 		userPosteInfos[0].addEventListener("click", function(event) {
 			switch (getCampus()) {
@@ -36,6 +36,9 @@ if (window.location.pathname.indexOf("/users/") == 0 || (window.location.hostnam
 				case "Paris":
 					window.open("https://stud42.fr/clusters#"+event.target.textContent);
 					break;
+			}
+			if (userPosteInfos[0].textContent.indexOf(".codam.nl") > -1) {
+				window.open("https://codamhero.dev/v2/clusters.php#"+event.target.textContent.split(".")[0]);
 			}
 		});
 	}
