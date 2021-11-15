06aba9dfc226e3f4131253b12c1d46c4d1162ceb
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2021-11-15 16:28:37 +0100

    now using 7-Zip for build

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ if [[ $OSTYPE == 'darwin'* ]]; then
 	mv manifest.json manifest-ff.json
 	mv manifest-chromium.json manifest.json
 else
-	tar.exe -a -c -f chromium.zip manifest.json LICENSE dark.css improv.js images/logo*.png _locales/*
+	7z a chromium.zip manifest.json LICENSE dark.css improv.js images/logo*.png _locales/*
 	mv manifest.json manifest-chromium.json
 	mv manifest-ff.json manifest.json
-	tar.exe -a -c -f firefox.zip manifest.json LICENSE dark.css improv.js images/logo*.png _locales/*
+	7z a firefox.zip manifest.json LICENSE dark.css improv.js images/logo*.png _locales/*
 	mv manifest.json manifest-ff.json
 	mv manifest-chromium.json manifest.json
 fi
