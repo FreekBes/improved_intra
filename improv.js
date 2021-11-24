ab22b286261e9c216670b14332db3ace8eaa30f9
Freek Bes <freekbladnl@gmail.com>
2021-11-24 18:38:59 +0100

    now opening cluster map on enter when user poste infos is focused

--- improv.js
+++ improv.js
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/13 00:37:55 by fbes          #+#    #+#                 */
-/*   Updated: 2021/11/24 18:34:57 by fbes          ########   odam.nl         */
+/*   Updated: 2021/11/24 18:38:37 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,48 @@ function unsetCoalitionTextColor(event) {
 	event.target.style.color = null;
 }
 
+function openLocationMap(event) {
+	var win = null;
+	var url = null;
+
+	switch (getCampus()) {
+		case "Amsterdam":
+			url = "https://codamhero.dev/v2/clusters.php";
+			break;
+		case "Paris":
+			url = "https://stud42.fr/clusters";
+			break;
+		default: {
+			if (userPosteInfos.textContent.indexOf(".codam.nl") > -1) {
+				url = "https://codamhero.dev/v2/clusters.php";
+			}
+			else {
+				url = "https://meta.intra.42.fr/clusters";
+			}
+			break;
+		}
+	}
+	win = window.open(url, "dark_intra_cluster_map_win");
+	// since we can no longer check when a window is loaded with an event
+	// for domains that are not of the same origin, we simply try and send
+	// the location ID multiple times to the opened cluster map window
+	setTimeout(function() {
+		win.location.href = url + "#" + event.target.textContent.split(".")[0];
+	}, 250);
+	setTimeout(function() {
+		win.location.href = url + "#";
+		win.location.href = url + "#" + event.target.textContent.split(".")[0];
+	}, 500);
+	setTimeout(function() {
+		win.location.href = url + "#";
+		win.location.href = url + "#" + event.target.textContent.split(".")[0];
+	}, 750);
+	setTimeout(function() {
+		win.location.href = url + "#";
+		win.location.href = url + "#" + event.target.textContent.split(".")[0];
+	}, 1000);
+}
+
 if (window.location.pathname.indexOf("/users/") == 0 || (window.location.hostname == "profile.intra.42.fr" && window.location.pathname == "/")) {
 	var userPosteInfos = document.querySelector(".user-poste-infos");
 	var profileActions = document.querySelector(".profile-item .user-primary .user-infos .button-actions");
@@ -62,46 +104,11 @@ if (window.location.pathname.indexOf("/users/") == 0 || (window.location.hostnam
 		userPosteInfos.setAttribute("tabindex", "0");
 		userPosteInfos.addEventListener("mouseenter", setCoalitionTextColor);
 		userPosteInfos.addEventListener("mouseleave", unsetCoalitionTextColor);
-		userPosteInfos.addEventListener("click", function(event) {
-			var win = null;
-			var url = null;
-
-			switch (getCampus()) {
-				case "Amsterdam":
-					url = "https://codamhero.dev/v2/clusters.php";
-					break;
-				case "Paris":
-					url = "https://stud42.fr/clusters";
-					break;
-				default: {
-					if (userPosteInfos.textContent.indexOf(".codam.nl") > -1) {
-						url = "https://codamhero.dev/v2/clusters.php";
-					}
-					else {
-						url = "https://meta.intra.42.fr/clusters";
-					}
-					break;
-				}
+		userPosteInfos.addEventListener("click", openLocationMap);
+		userPosteInfos.addEventListener("keyup", function(event) {
+			if (event.keyCode == 13) {
+				openLocationMap(event);
 			}
-			win = window.open(url, "dark_intra_cluster_map_win");
-			// since we can no longer check when a window is loaded with an event
-			// for domains that are not of the same origin, we simply try and send
-			// the location ID multiple times to the opened cluster map window
-			setTimeout(function() {
-				win.location.href = url + "#" + event.target.textContent.split(".")[0];
-			}, 250);
-			setTimeout(function() {
-				win.location.href = url + "#";
-				win.location.href = url + "#" + event.target.textContent.split(".")[0];
-			}, 500);
-			setTimeout(function() {
-				win.location.href = url + "#";
-				win.location.href = url + "#" + event.target.textContent.split(".")[0];
-			}, 750);
-			setTimeout(function() {
-				win.location.href = url + "#";
-				win.location.href = url + "#" + event.target.textContent.split(".")[0];
-			}, 1000);
 		});
 	}
 }
