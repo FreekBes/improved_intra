311f7a68709407f666bdee151b34c6c97e2ed0ba
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2021-11-28 23:17:03 +0100

    rm firefox chromium zips

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [[ $OSTYPE == 'darwin'* ]]; then
+	rm -f chromium.zip firefox.zip
 	zip -r chromium.zip manifest.json LICENSE js/* options/* css/* campus_specifics/* images/logo*.png _locales/*
 	mv manifest.json manifest-chromium.json
 	mv manifest-ff.json manifest.json
@@ -7,6 +8,7 @@ if [[ $OSTYPE == 'darwin'* ]]; then
 	mv manifest.json manifest-ff.json
 	mv manifest-chromium.json manifest.json
 else
+	rm -f chromium.zip firefox.zip
 	7z a chromium.zip manifest.json LICENSE js/* options/* css/* campus_specifics/* images/logo*.png _locales/*
 	mv manifest.json manifest-chromium.json
 	mv manifest-ff.json manifest.json
