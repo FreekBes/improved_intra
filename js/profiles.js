18036cb71e6d463a6726b1b81ed8abfcfc397ab2
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-01-13 21:15:10 +0100

    fix #11 by checking the banner image url in an interval

--- js/profiles.js
+++ js/profiles.js
@@ -24,8 +24,18 @@ function getProfileUserName() {
 	}
 }
 
+var gUName = null;
+var gProfileBanner = null;
+var gInterval = null;
+var gExtSettings = null;
+var gUserSettings = null;
+
 function getUserSettings(username) {
 	return new Promise(function(resolve, reject) {
+		if (gUserSettings && gUserSettings["username"] === username) {
+			resolve(gUserSettings);
+			return;
+		}
 		console.log("Retrieving settings of username " + username);
 		fetch("https://darkintra.freekb.es/settings/" + username + ".json?noCache=" + Math.random())
 			.then(function(response) {
@@ -43,6 +53,7 @@ function getUserSettings(username) {
 					reject();
 				}
 				else {
+					gUserSettings = json;
 					resolve(json);
 				}
 			})
@@ -53,76 +64,83 @@ function getUserSettings(username) {
 }
 
 
-function setCustomBanner(profileBanner, imageUrl, imagePos) {
+function setCustomBanner(imageUrl, imagePos) {
 	if (imageUrl && validateUrl(imageUrl)) {
-		profileBanner.className += " customized";
-		profileBanner.setAttribute("data-old-bg", profileBanner.style.backgroundImage);
-		profileBanner.style.backgroundImage = "url(" + imageUrl + ")";
-		switch (imagePos) {
-			default:
-			case "center-center":
-				profileBanner.style.backgroundPosition = "center center";
-				break;
-			case "center-top":
-				profileBanner.style.backgroundPosition = "center top";
-				break;
-			case "center-bottom":
-				profileBanner.style.backgroundPosition = "center bottom";
-				break;
+		var newCSSval = "url(\"" + imageUrl + "\")";
+		if (gProfileBanner.style.backgroundImage.indexOf(imageUrl) == -1) {
+			gProfileBanner.className += " customized";
+			gProfileBanner.setAttribute("data-old-bg", gProfileBanner.style.backgroundImage);
+			gProfileBanner.style.backgroundImage = newCSSval;
+			switch (imagePos) {
+				default:
+				case "center-center":
+					gProfileBanner.style.backgroundPosition = "center center";
+					break;
+				case "center-top":
+					gProfileBanner.style.backgroundPosition = "center top";
+					break;
+				case "center-bottom":
+					gProfileBanner.style.backgroundPosition = "center bottom";
+					break;
+			}
+			console.log("Custom banner set!");
 		}
-		console.log("Custom banner set!");
 		return (true);
 	}
 	return (false);
 }
 
-function unsetCustomBannerIfRequired(profileBanner) {
-	if (profileBanner.getAttribute("data-old-bg")) {
-		profileBanner.style.backgroundImage = profileBanner.getAttribute("data-old-bg");
-		profileBanner.removeAttribute("data-old-bg");
+function unsetCustomBannerIfRequired() {
+	if (gProfileBanner.getAttribute("data-old-bg")) {
+		gProfileBanner.style.backgroundImage = gProfileBanner.getAttribute("data-old-bg");
+		gProfileBanner.removeAttribute("data-old-bg");
 		console.log("Custom banner unset");
 	}
 }
 
 function setCustomProfile() {
-	var uName = getProfileUserName();
-	var profileBanner = document.querySelector(".container-inner-item.profile-item-top.profile-banner");
-	chrome.storage.local.get(["username", "show-custom-profiles", "custom-banner-url", "custom-banner-pos"], function(data) {
-		if (data["show-custom-profiles"] === true || data["show-custom-profiles"] === "true") {
-			if (profileBanner) {
-				if (uName == data["username"]) {
-					if (!setCustomBanner(profileBanner, data["custom-banner-url"], data["custom-banner-pos"])) {
-						unsetCustomBannerIfRequired(profileBanner);
-					}
-				}
-				else {
-					getUserSettings(uName)
-						.then(function(uSettings) {
-							if (!setCustomBanner(profileBanner, uSettings["custom-banner-url"], uSettings["custom-banner-pos"])) {
-								unsetCustomBannerIfRequired(profileBanner);
-							}
-						})
-						.catch(function(err) {
-							// no custom profile settings found
-						});
+	if (gExtSettings["show-custom-profiles"] === true || gExtSettings["show-custom-profiles"] === "true") {
+		if (gProfileBanner) {
+			if (gUName == gExtSettings["username"]) {
+				if (!setCustomBanner(gExtSettings["custom-banner-url"], gExtSettings["custom-banner-pos"])) {
+					unsetCustomBannerIfRequired();
 				}
 			}
-		}
-	});
-}
-
-function immediateProfileChanges() {
-	// easter egg for user fbes, even when customized profiles are disabled
-	if (getProfileUserName() == "fbes") {
-		var profileBanner = document.querySelector(".container-inner-item.profile-item-top.profile-banner");
-		if (profileBanner) {
-			profileBanner.className += " egg";
+			else {
+				getUserSettings(gUName)
+					.then(function(uSettings) {
+						if (!setCustomBanner(uSettings["custom-banner-url"], uSettings["custom-banner-pos"])) {
+							unsetCustomBannerIfRequired();
+						}
+					})
+					.catch(function(err) {
+						// no custom profile settings found
+					});
+			}
 		}
 	}
 }
 
+function immediateProfileChanges() {
+	// easter egg for user fbes, even when customized profiles are disabled
+	if (gUName == "fbes") {
+		if (gProfileBanner) {
+			gProfileBanner.className += " egg";
+		}
+	}
+}
+
+gUName = getProfileUserName();
+gProfileBanner = document.querySelector(".container-inner-item.profile-item-top.profile-banner");
 immediateProfileChanges();
-// set custom profile with timeout, as sometimes the coalition banner takes a while to load
-setTimeout(function() {
+chrome.storage.local.get(["username", "show-custom-profiles", "custom-banner-url", "custom-banner-pos"], function(data) {
+	gExtSettings = data;
 	setCustomProfile();
-}, 150);
+	// check if the custom profile is kept in an interval for 5 seconds
+	// sometimes things get overruled by coalition stuff, such as banners
+	gInterval = setInterval(setCustomProfile, 150);
+	setTimeout(function() {
+		clearInterval(gInterval);
+		gInterval = null;
+	}, 5000);
+});
