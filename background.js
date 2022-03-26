03ba476e64125747fff72efd9bdb176893b43889
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-03-26 23:54:45 +0100

    not done, but important push
    before splitting up background.js

--- background.js
+++ background.js
@@ -10,6 +10,220 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// sadly this file cannot be split up right now, because Firefox doesn't currently
+// support service workers as background scripts yet, so we cannot use the importScripts
+// functionality. Everything in this file is split up by comments instead.
+// Once Firefox implements service workers, everything should be easily split-uppable.
+
+
+// GENERAL_BACKGROUND_JS ///////////////////////////////////////////////////////
+
+// (keep everything in this section on split-up)
+chrome.runtime.onInstalled.addListener(function(details) {
+	if (details.reason == "install") {
+		console.log("First install.");
+		// setSettingsIfUnset();
+		// fetchUserSettings();
+	}
+	else if (details.reason == "update") {
+		console.log("An update has been installed. Run getSettingsFromSyncServer(username) to force synchronization with the sync server.");
+		// removeUnusedSettings();
+		// setSettingsIfUnset();
+	}
+});
+
+// END GENERAL_BACKGROUND_JS ///////////////////////////////////////////////////
+
+
+
+
+
+// START STORAGE ///////////////////////////////////////////////////////////////
+
+/*
+To start, there's three storages:
+- a shared one, for generic usage
+- a normal one, where user session storage can be stored
+- an incognito one, where user session storage from the incognito browser can be stored
+Each storage is separated by STORAGETYPE_ in front of the key originally used for the storage.
+EXAMPLE: if one were to fetch something with the key "test"; use "SHARED_test", "NORMAL_test", "INCOGNITO_test" instead.
+
+The reason behind this is because storage is actually always shared between normal and incognito mode...
+Which is not useful in our case, where we often use storage to synchronize settings based on the user who's currently
+signed in to intra.42.fr. This can of course differ between normal and incognito mode
+as cookies (and thus sessions) are NOT shared between these modes!
+*/
+
+// helper function to prepare keys for storage use
+function prepareStorageKeys(type, keys) {
+	if (typeof keys == "string") {
+		keys = [keys];
+	}
+	for (let i = 0; i < keys.length; i++) {
+		keys[i] = type.toUpperCase() + "-" + keys[i];
+	}
+	return (keys);
+}
+
+// helper function to sanitize storage returned data
+function sanitizeStorageData(orig_keys, internal_keys, data) {
+	const newData = {};
+
+	for (let i = 0; i < internal_keys.length; i++) {
+		newData[orig_keys[i]] = data[internal_keys[i]];
+	}
+	return (newData);
+}
+
+// get something from storage by type
+function getStorage(type, keys) {
+	return new Promise(function (resolve) {
+		const internal_keys = prepareStorageKeys(type, keys);
+		chrome.storage.local.get(keys, function(data) {
+			resolve(sanitizeStorageData(keys, internal_keys, data));
+		});
+	});
+}
+
+// wrapper for getStorage("shared", keys);
+function getSharedStorage(keys) {
+	return (getStorage("shared", keys));
+}
+
+// wrapper for getStorage("incognito", keys);
+function getIncognitoStorage(keys) {
+	return (getStorage("incognito", keys));
+}
+
+// wrapper for getStorage("normal", keys);
+function getNormalStorage(keys) {
+	return (getStorage("normal", keys));
+}
+
+// END STORAGE /////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// START COMMUNICATION_SERVICE /////////////////////////////////////////////////
+
+const incognitoPortName = "incog_comm";
+const normalPorts = [];
+const incognitoPorts = [];
+
+// message only ports in non-incognito context
+function messageNormalPorts(msg) {
+	for (let i = 0; i < ports.length; i++) {
+		normalPorts[i].postMessage(msg);
+	}
+}
+
+// message only ports in incognito context
+function messageIncognitoPorts(msg) {
+	for (let i = 0; i < ports.length; i++) {
+		incognitoPorts[i].postMessage(msg);
+	}
+}
+
+// message all ports
+function messageAllPorts(msg) {
+	messageNormalPorts(msg);
+	messageIncognitoPorts(msg);
+}
+
+// detect if port is incognito or not
+function isIncognitoPort(port) {
+	return (port.name == incognitoPortName);
+}
+
+// resync function
+function resync(port, incognitoSession) {
+	const storageType = (incognitoSession ? "incognito" : "normal");
+
+	getStorage(storageType, "username").then(function(data) {
+		if (data["username"]) {
+			getSettingsFromSyncServer(storageType, data["username"])
+				.then(function() {
+					console.log("Settings successfully retrieved from server. Stored a copy locally.");
+					if (incognitoSession) {
+						messageIncognitoPorts({ action: "resynced" });
+					}
+					else {
+						messageNormalPorts({ action: "resynced" });
+					}
+				})
+				.catch(function(err) {
+					resetSettings().then(function() {
+						if (incognitoSession) {
+							messageIncognitoPorts({ action: "resynced" });
+						}
+						else {
+							messageNormalPorts({ action: "resynced" });
+						}
+					});
+				});
+		}
+		else {
+			console.warn("Could not resync, as no username was given to sync from");
+		}
+	});
+}
+
+// port message listener
+function portMessageListener(msg, port) {
+	switch(msg["action"]) {
+		case "ping":
+			port.postMessage({ action: "pong" });
+			break;
+		case "resync":
+			resync(port, isIncognitoPort(port));
+			break;
+		case "options-changed":
+			messageAllPorts({ action: "options-changed", settings: msg["settings"] });
+			break;
+		default:
+			console.log("Unknown action received over port: ", msg["action"]);
+			break;
+	}
+}
+
+chrome.runtime.onConnect.addListener(function(port) {
+	// add port to array of ports depending on the name
+	if (port.name == incognitoPortName) {
+		incognitoPorts.push(port);
+	}
+	else {
+		normalPorts.push(port);
+	}
+
+	// remove port from array of ports on closure
+	port.onDisconnect.addListener(function(port) {
+		const normalIndex = normalPorts.indexOf(port);
+		const incognitoIndex = incognitoPorts.indexOf(port);
+
+		if (incognitoIndex > -1) {
+			incognitoPorts.splice(incognitoIndex, 1);
+		}
+		else if (normalIndex > -1) {
+			normalPorts.splice(normalIndex, 1);
+		}
+	});
+
+	// run specific functions on specific messages
+	// basically treating messages as commands
+	port.onMessage.addListener(portMessageListener);
+});
+
+// END COMMUNICATION_SERVICE ///////////////////////////////////////////////////
+
+
+
+
+
+// START OLD_BACKGROUND_JS /////////////////////////////////////////////////////
+
 var defaultSettings = {
 	"clustermap": "true",
 	"codam-auto-equip-coa-title": "false",
@@ -101,7 +315,7 @@ function setSettingsIfUnset() {
 	});
 }
 
-function getSettingsFromSyncServer(username) {
+function getSettingsFromSyncServer(storageType, username) {
 	return new Promise(function(resolve, reject) {
 		console.log("Retrieving settings of username " + username);
 		fetch("https://darkintra.freekb.es/settings/" + username + ".json?noCache=" + Math.random())
@@ -131,51 +345,6 @@ function getSettingsFromSyncServer(username) {
 	});
 }
 
-var ports = [];
-function messageAllPorts(msg) {
-	for (var i = 0; i < ports.length; i++) {
-		ports[i].postMessage(msg);
-	}
-}
-chrome.runtime.onConnect.addListener(function(port) {
-	ports.push(port);
-	port.onDisconnect.addListener(function(port) {
-		ports.splice(ports.indexOf(port), 1);
-	});
-	port.onMessage.addListener(function(msg) {
-		switch(msg["action"]) {
-			case "ping":
-				port.postMessage({ action: "pong" });
-				break;
-			case "resync":
-				chrome.storage.local.get("username", function(data) {
-					if (data["username"]) {
-						getSettingsFromSyncServer(data["username"])
-							.then(function() {
-								console.log("Settings successfully retrieved from server. Stored a copy locally.");
-								messageAllPorts({ action: "resynced" });
-							})
-							.catch(function(err) {
-								resetSettings().then(function() {
-									messageAllPorts({ action: "resynced" });
-								});
-							});
-					}
-					else {
-						console.warn("Could not resync, as no username was given to sync from");
-					}
-				});
-				break;
-			case "options-changed":
-				messageAllPorts({ action: "options-changed", settings: msg["settings"] });
-				break;
-			default:
-				console.log("Unknown action received over port: ", msg["action"]);
-				break;
-		}
-	});
-});
-
 function fetchUserSettings() {
 	tryFetchIntraUsername()
 			.then(getSettingsFromSyncServer)
@@ -187,15 +356,4 @@ function fetchUserSettings() {
 			});
 }
 
-chrome.runtime.onInstalled.addListener(function(details) {
-	if (details.reason == "install") {
-		console.log("First install.");
-		setSettingsIfUnset();
-		fetchUserSettings();
-	}
-	else if (details.reason == "update") {
-		console.log("An update has been installed. Run getSettingsFromSyncServer(username) to force synchronization with the sync server.");
-		removeUnusedSettings();
-		setSettingsIfUnset();
-	}
-});
+// END OLD_BACKGROUND_JS ///////////////////////////////////////////////////////
