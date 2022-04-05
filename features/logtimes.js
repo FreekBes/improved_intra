c699b0342a3e3cd5f7ee634c1bce3987993f1f15
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-04-05 23:39:41 +0200

    add sum of monthly logtime

--- features/logtimes.js
+++ features/logtimes.js
@@ -17,9 +17,92 @@ function codamMonitHelper(settings, logTime) {
 	return ("");
 }
 
+// month logtime has to be calculated from the web since some days may be missing from the logtimes chart
+function sumMonthLogTime(ltMonths) {
+	const httpReq = new XMLHttpRequest();
+	httpReq.addEventListener("load", function() {
+		try {
+			const stats = JSON.parse(this.responseText);
+			const dates = Object.keys(stats);
+			const monthSums = [];
+			let mIndex = -1;
+			let lastMonth = -1;
+			for (const date of dates) {
+				const month = parseInt(date.split("-")[1]);
+				if (month != lastMonth) {
+					lastMonth = month;
+					mIndex++;
+					monthSums.push(0);
+				}
+				monthSums[mIndex] += parseLogTime(stats[date]);
+			}
+			ltMonths = Array.from(ltMonths).reverse();
+			for (let i = 0; i < ltMonths.length; i++) {
+				const oldBbox = ltMonths[i].getBBox();
+				const oldX = parseInt(ltMonths[i].getAttribute("x"));
+				ltMonths[i].textContent = ltMonths[i].textContent + " (" + logTimeToString(monthSums[i]) + ")";
+				const newBbox = ltMonths[i].getBBox();
+				// move element's x coordinate to the left to account for the width of the text added
+				ltMonths[i].setAttribute("x", Math.round(oldX - (newBbox.width - oldBbox.width) * 0.5));
+			}
+		}
+		catch (err) {
+			iConsole.error(err);
+		}
+	});
+	httpReq.addEventListener("error", function(err) {
+		iConsole.error(err);
+	});
+	httpReq.open("GET", window.location.origin + "/users/" + getProfileUserName() + "/locations_stats.json");
+	httpReq.send();
+}
+
+function cumWeekLogTime(ltDays, settings) {
+	let ltDay = ltDays[ltDays.length - 1];
+	let daysInWeek = dayOfWeek + 1;
+	const remainingWeeks = Math.floor(ltDays.length / 7) + (dayOfWeek != 6 ? 1 : 0);
+	let r = 0;
+	for (let i = 0; i < remainingWeeks; i++) {
+		let j;
+
+		if (i == 1) {
+			daysInWeek = 7;
+		}
+		const tempLogTimes = [];
+
+		// parse individual logtimes
+		for (j = 0; j < daysInWeek; j++) {
+			ltDay = ltDays[ltDays.length - r - 1];
+			if (!ltDay) {
+				return;
+			}
+			tempLogTimes.push(parseLogTime(ltDay.getAttribute("data-original-title")));
+			if (tempLogTimes[j] == 0) {
+				ltDay.setAttribute("data-nolog", "");
+			}
+			r++;
+		}
+
+		// calculate cumulative logtime
+		for (j = daysInWeek - 2; j > -1; j--) {
+			tempLogTimes[j] = tempLogTimes[j] + tempLogTimes[j + 1];
+		}
+
+		// add cumulative logtime to tooltips (and percentage if Codam Monit System enabled)
+		for (j = daysInWeek - 1; j > -1; j--) {
+			ltDay = ltDays[ltDays.length - r + j];
+			if (!ltDay) {
+				return;
+			}
+			ltDay.setAttribute("data-original-title", ltDay.getAttribute("data-original-title") + " (week's cumulative: " + logTimeToString(tempLogTimes[daysInWeek - 1 - j]) + codamMonitHelper(settings, tempLogTimes[daysInWeek - 1 - j]) + ")");
+		}
+	}
+}
+
 function waitForLogTimesChartToLoad(ltSvg) {
 	const ltDays = ltSvg.getElementsByTagName("g");
-	if (ltDays.length < 1) {
+	const ltMonths = ltSvg.querySelectorAll("svg > text");
+	if (ltDays.length == 0 || ltMonths.length == 0) {
 		// logtimes chart hasn't finished loading yet, try again in 100ms
 		setTimeout(function() {
 			waitForLogTimesChartToLoad(ltSvg);
@@ -33,7 +116,6 @@ function waitForLogTimesChartToLoad(ltSvg) {
 		viewBox = viewBox.split(" ").map(function(item) {
 			return parseInt(item);
 		});
-		console.log(viewBox);
 		if (viewBox[0] > 0) {
 			viewBox[0] = 0;
 			ltSvg.setAttribute("viewBox", viewBox.join(" "));
@@ -41,45 +123,11 @@ function waitForLogTimesChartToLoad(ltSvg) {
 	}
 
 	improvedStorage.get(["logsum-month", "logsum-week", "codam-monit"]).then(function(settings) {
-		let ltDay = ltDays[ltDays.length - 1];
-		let daysInWeek = dayOfWeek + 1;
-		const remainingWeeks = Math.floor(ltDays.length / 7) + (dayOfWeek != 6 ? 1 : 0);
-		let r = 0;
-		const logTimes = [];
-		for (let i = 0; i < remainingWeeks; i++) {
-			let j;
-
-			if (i == 1) {
-				daysInWeek = 7;
-			}
-			const tempLogTimes = [];
-
-			// parse individual logtimes
-			for (j = 0; j < daysInWeek; j++) {
-				ltDay = ltDays[ltDays.length - r - 1];
-				if (!ltDay) {
-					return true;
-				}
-				tempLogTimes.push(parseLogTime(ltDay.getAttribute("data-original-title")));
-				if (tempLogTimes[j] == 0) {
-					ltDay.setAttribute("data-nolog", "");
-				}
-				r++;
-			}
-
-			// calculate cumulative logtime
-			for (j = daysInWeek - 2; j > -1; j--) {
-				tempLogTimes[j] = tempLogTimes[j] + tempLogTimes[j + 1];
-			}
-
-			// add cumulative logtime to tooltips (and percentage if Codam Monit System enabled)
-			for (j = daysInWeek - 1; j > -1; j--) {
-				ltDay = ltDays[ltDays.length - r + j];
-				if (!ltDay) {
-					return true;
-				}
-				ltDay.setAttribute("data-original-title", ltDay.getAttribute("data-original-title") + " (week's cumulative: " + logTimeToString(tempLogTimes[daysInWeek - 1 - j]) + codamMonitHelper(settings, tempLogTimes[daysInWeek - 1 - j]) + ")");
-			}
+		if (settings["logsum-month"]) {
+			sumMonthLogTime(ltMonths);
+		}
+		if (settings["logsum-week"]) {
+			cumWeekLogTime(ltDays, settings);
 		}
 	});
 }
