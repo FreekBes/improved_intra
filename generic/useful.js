6074510f7ccbdb3f2af296fa8256195129ef5905
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2023-02-09 14:40:47 +0100

    add fallback for getCoalitionColor()

--- generic/useful.js
+++ generic/useful.js
@@ -15,14 +15,22 @@ function validateUrl(value) {
 	return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
 }
 
+// get the theme color of the Intranet
+function getThemeColor() {
+	const color = getComputedStyle(document.documentElement).getPropertyValue('--theme-color');
+	return (color ? color : '#00babc'); // fallback
+}
+
 // get the coalition color of a user or coalition on a webpage
 function getCoalitionColor() {
+	let color = getThemeColor(); // fallback
 	try {
-		return (document.getElementsByClassName("coalition-span")[0].style.color);
+		color = document.getElementsByClassName("coalition-span")[0].style.color;
 	}
 	catch (err) {
-		return ("#FF0000");
+		iConsole.warn("Could not get coalition color, using theme color instead.");
 	}
+	return (color);
 }
 
 // get the name of the campus a user is part of (only on profile pages)
