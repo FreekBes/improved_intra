672d7ac79d3e68bf841c7516018fef111e42931b
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2025-07-19 18:39:11 +0200

    add custom banner to user profiles

--- src/v3/profiles/scripts.js
+++ src/v3/profiles/scripts.js
@@ -65,8 +65,8 @@ const ProfileV3 = {
 		iConsole.log("Initializing Profile V3 improvements");
 
 		// Check for newly created nodes with a MutationObserver.
-		// Compare every node created with the loadDetector of each box.
-		const observer = new MutationObserver((mutations) => {
+		// Compare every node created with the loadDetector of each Profile box.
+		const boxesObserver = new MutationObserver((mutations) => {
 			mutations.forEach((mutation) => {
 				if (mutation.addedNodes) {
 					mutation.addedNodes.forEach((node) => {
@@ -84,7 +84,7 @@ const ProfileV3 = {
 								ProfileV3.profile_boxes_found++;
 								if (ProfileV3.profile_boxes_found === ProfileV3.profile_boxes.length) {
 									iConsole.log("All Profile V3 boxes found, disconnecting observer");
-									observer.disconnect();
+									boxesObserver.disconnect();
 								}
 
 								// Apply iintra-profile-bg class to the container of the boxes
@@ -100,7 +100,114 @@ const ProfileV3 = {
 			});
 		});
 
+		// Check for newly created header nodes with a MutationObserver.
+		const headerObserver = new MutationObserver((mutations) => {
+			mutations.forEach((mutation) => {
+				if (mutation.addedNodes) {
+					mutation.addedNodes.forEach((node) => {
+						const header = node.querySelector("header");
+						if (header) {
+							iConsole.log("Profile header has been added to the page!");
+							// Header has been added, disconnect the MutationObserver.
+							headerObserver.disconnect();
+							ProfileV3.setupProfileHeader(header);
+						}
+					});
+				}
+			});
+		});
+
 		// Start observing the body for changes.
-		observer.observe(document.body, { childList: true, subtree: true });
+		boxesObserver.observe(document.body, { childList: true, subtree: true });
+		headerObserver.observe(document.body, { childList: true, subtree: true });
+	},
+
+	/**
+	 * Get the login of the profile currently being viewed.
+	 * @returns {string} The login of the profile.
+	 */
+	getProfileLogin: () => {
+		// Login is in the URL, as the last part of the path
+		const parts = window.location.pathname.split("/");
+		return parts[parts.length - 1];
+	},
+
+	/**
+	 * Get a user's profile settings from the Improved Intra server.
+	 * @param {string} login The username of the user to retrieve the profile of.
+	 * @returns {Promise<Object>} A promise that resolves with the user's profile data or rejects with an error message.
+	 */
+	getUserProfile: (login) => {
+		return new Promise(async function(resolve, reject) {
+			iConsole.log("Retrieving profile of login " + login);
+			try {
+				const res = await fetch("https://iintra.freekb.es/v2/profile/" + login + ".json", {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+						"Accept": "application/json",
+					},
+				});
+				if (!res.ok) {
+					if (res.status === 404) {
+						reject(`No profile found for login ${login}, user is likely not an Improved Intra user.`);
+						return;
+					}
+
+					reject("Failed to fetch profile data: " + res.status + " " + res.statusText);
+					return;
+				}
+				const json = await res.json();
+				if (json["type"] == "success") {
+					resolve(json["data"]);
+				}
+				else {
+					reject(json["message"]);
+				}
+			}
+			catch (err) {
+				reject(err);
+			}
+		});
+	},
+
+	setupProfileBannerImage: (header, profileData) => {
+		// Apply background image to the first child of the header.
+		// The original header will have the original background image as set by Intra.
+		// The child will then overlay our header. In case our header fails to load, the original header will still be visible.
+		const customBannerElement = header.firstElementChild;
+		if (!customBannerElement) {
+			iConsole.error("No child element found in the profile header to apply the banner image.");
+			return;
+		}
+
+		customBannerElement.classList.add("iintra-profile-banner");
+		if (!profileData.banner_img || !profileData.banner_img.url) {
+			iConsole.log("No banner image found in the profile data.");
+			customBannerElement.style.backgroundImage = null; // Clear the background image if no banner is set
+			return;
+		}
+		customBannerElement.style.backgroundImage = `url(${profileData.banner_img.url})`;
+		customBannerElement.style.backgroundPosition = profileData.banner_pos.css_val;
+	},
+
+	/**
+	 * Setup the profile header with the necessary improvements.
+	 * @param {Element} header The header element to improve.
+	 * @returns {void}
+	 */
+	setupProfileHeader: async (header) => {
+		const login = ProfileV3.getProfileLogin();
+		try {
+			const profileData = await ProfileV3.getUserProfile(login);
+			if (!profileData) {
+				iConsole.error("Failed to retrieve profile data for " + login);
+				return;
+			}
+			iConsole.log("Setting up profile header for " + login, profileData);
+			ProfileV3.setupProfileBannerImage(header, profileData);
+		} catch (error) {
+			iConsole.error("Error setting up profile for " + login, error);
+		}
 	},
 }
